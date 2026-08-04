ec0e9cd89d822c221bd8f587274b056a04304ffc
HabibAsif <habibulalamasif1@gmail.com>
2026-08-04 18:22:47 +0600

    Operator handling

--- js/lexer.js
+++ js/lexer.js
@@ -10,6 +10,16 @@ export function tokenize(text) {
     function atEnd() { return i >= text.length }
     function curr() { return text[i] }
 
+    function appendWord() {
+        tokenArr.push(token)
+        token = new Token("", 0)
+    }
+
+    function appendOperator(type) {
+        appendWord()
+        tokenArr.push(new Token(curr(), type))
+    }
+
     let i = 0
 
     let token = new Token("", 0)
@@ -24,9 +34,15 @@ export function tokenize(text) {
             case '\v':
                 if (token.val === "")  { break }
 
-                tokenArr.push(token)
-                token = new Token("", 0)
+                appendWord()
                 break
+            
+            case ':':     appendOperator(TokenType.COLON); break
+            case ',':     appendOperator(TokenType.COMMA); break
+            case '.':     appendOperator(TokenType.DOT); break
+            
+            case '"':
+            case '\'':    appendOperator(TokenType.QUOTE); break 
             
             default:
                 token.val += curr()
@@ -35,8 +51,12 @@ export function tokenize(text) {
         i++
     }
 
+    if (token.val !== "") { appendWord() }
+
     return tokenArr
 }
 
 // test
-tokenize("placeholder:  Placeholder variable name for prototyping: foo, bar, baz").forEach(token => console.log(token.val));
+tokenize(
+    "placeholder  Placeholder variable name for prototyping: foo, bar, baz."
+).forEach(token => console.log(`${token.val} | ${token.type}`));
