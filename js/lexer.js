d82062e27b676157c9b2825c8cb62fccd69aadaf
HabibAsif <habibulalamasif1@gmail.com>
2026-08-04 18:08:32 +0600

    Lexer split on whitespace

--- js/lexer.js
+++ js/lexer.js
@@ -1,1 +1,42 @@
 import {Token, TokenType} from './token.js'
+
+
+/**
+ * @param {string} text
+ * @returns {Token[]}
+*/
+export function tokenize(text) {
+
+    function atEnd() { return i >= text.length }
+    function curr() { return text[i] }
+
+    let i = 0
+
+    let token = new Token("", 0)
+    let tokenArr = []
+
+    while (!atEnd()) {
+        switch (curr()) {
+            case ' ':
+            case '\n':
+            case '\r':
+            case '\t':
+            case '\v':
+                if (token.val === "")  { break }
+
+                tokenArr.push(token)
+                token = new Token("", 0)
+                break
+            
+            default:
+                token.val += curr()
+        }
+
+        i++
+    }
+
+    return tokenArr
+}
+
+// test
+tokenize("placeholder:  Placeholder variable name for prototyping: foo, bar, baz").forEach(token => console.log(token.val));
