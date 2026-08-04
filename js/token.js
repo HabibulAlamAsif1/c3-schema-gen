570c1697c8e3ee5f1e7e931923d341c7538c6fda
HabibAsif <habibulalamasif1@gmail.com>
2026-08-04 17:44:42 +0600

    Added token types

---
+++ js/token.js
@@ -0,0 +1,22 @@
+const TokenType = Object.freeze({
+    WORD: 0,
+    KW_DEFAULT: 1,
+    KW_ENABLE: 2,
+    KW_DISABLE: 3,
+    KW_ENABLE_DISABLE: 4,
+    KW_ON: 5,
+    KW_OFF: 6,
+    KW_DIRECTORY: 7,
+    KW_PATH: 8,
+    KW_FILE: 9,
+    KW_FLAG: 10,
+    KW_OR: 11,
+    KW_AND: 12,
+    KW_ONE: 13,
+    KW_OF: 14,
+    COMMA: 15,
+    COLON: 16,
+    DOT: 17,
+    QUOTE: 18
+})
+
