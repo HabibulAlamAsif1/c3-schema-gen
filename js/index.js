5c2340fe0d6017ef7977d80247e0a5ef0a84d606
HabibAsif <habibulalamasif1@gmail.com>
2026-08-05 20:39:02 +0600

    Input validation for empty input

--- js/index.js
+++ js/index.js
@@ -10,7 +10,31 @@ const outputBox = document.getElementById("output-box")
 const generateBtn = document.getElementById("generate")
 
 
+function validateInput() {
+    if (inputBoxProps.value.length === 0) { return "Properties textbox is empty"}
+    if (inputBoxTargets.value.length === 0) { return "Targets textbox is empty"}
+
+    return ""
+}
+
+function inputError() {
+    outputBox.style.color = "#ff0000"
+}
+
+
 generateBtn.addEventListener("click", () => {
+
+    let err = validateInput()
+    if (err !== "") {
+        inputError()
+        outputBox.value = err
+        return
+    }
+
+    outputBox.style.color = "var(--text-clr)"
+
+    inputBoxProps.value += '\n'
+    inputBoxTargets.value += '\n'
 
     outputBox.value = JSON.stringify(generateSchemaObject(
         analize( splitTokens( tokenize( inputBoxProps.value))),
