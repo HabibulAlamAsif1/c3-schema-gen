74fc5b4994172860631e29ccbac8591a82abb4cc
HabibAsif <habibulalamasif1@gmail.com>
2026-08-06 23:29:36 +0600

    Made download button work

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ const inputBoxProps = document.getElementById("input-box-props")
 const inputBoxTargets = document.getElementById("input-box-targets")
 const outputBox = document.getElementById("output-box")
 const generateBtn = document.getElementById("generate")
+const downloadBtn = document.getElementById("download-btn")
 
 
 function validateInput() {
@@ -20,6 +21,13 @@ function inputError() {
     outputBox.style.color = "#ff0000"
 }
 
+
+outputBox.addEventListener("input", (e) => {
+    if (e.target.value === "") {
+        if (!downloadBtn.classList.contains("inactive")) { downloadBtn.classList.add("inactive") }
+        return
+    }
+})
 
 generateBtn.addEventListener("click", () => {
 
@@ -39,18 +47,23 @@ generateBtn.addEventListener("click", () => {
         analize( splitTokens( tokenize( inputBoxProps.value))),
         getTargetsList(inputBoxTargets.value)
     ), null, 2)
+
+    if (downloadBtn.classList.contains("inactive")) { downloadBtn.classList.remove("inactive") }
 })
 
 
 for (const pasteBtn of document.getElementsByClassName("paste-btn")) { pasteBtn.addEventListener("click", (e) => {
-    const textbox = e.target.parentElement.parentElement.querySelector("textarea")
+    const textbox = e.currentTarget.parentElement.parentElement.querySelector("textarea")
 
     navigator.clipboard.readText().then((value) => { textbox.value = value })
 }) }
 
 for (const clearBtn of document.getElementsByClassName("clear-btn")) { clearBtn.addEventListener("click", (e) => {
-    const textbox = e.target.parentElement.parentElement.querySelector("textarea")
+    /**@type {HTMLTextAreaElement} */
+    const textbox = e.currentTarget.parentElement.parentElement.querySelector("textarea")
+
     textbox.value = ""
+    textbox.dispatchEvent(new Event("input"))
 }) }
 
 document.getElementById("copy-btn").addEventListener("click", async (e) => {
@@ -59,4 +72,19 @@ document.getElementById("copy-btn").addEventListener("click", async (e) => {
     } catch (err) {
         console.log(err.message)
     }
+})
+
+downloadBtn.addEventListener("click", (e) => {
+    if (e.target.classList.contains("inactive")) { return }
+
+    const blob = new Blob([outputBox.value], { type: "application/json" })
+    const blobURL = URL.createObjectURL(blob)
+    const link = document.createElement("a")
+
+    link.href = blobURL
+    link.download = "c3-config.schema.json"
+    link.click()
+
+    URL.revokeObjectURL(blobURL)
+    link.remove()
 })
